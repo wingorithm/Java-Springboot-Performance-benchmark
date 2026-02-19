89502195e027afa2d0248dcbf7202bf69416d850
wingorithm <erwin.gunawan@binus.ac.id>
2026-02-19 16:15:04 +0700

    java 17 init

---
+++ java-17-service/src/main/java/java/benchmark/j17/Java17ServiceApplication.java
@@ -0,0 +1,13 @@
+package java.benchmark.j17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17ServiceApplication.class, args);
+	}
+
+}
---
+++ java-17-service/src/test/java/java/benchmark/j17/Java17ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package java.benchmark.j17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
